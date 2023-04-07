3ac1749a9f24e4ec5ab9c650538106be249f8393
Franck P <franckpoingt@gmail.com>
2023-04-07 14:44:48 +1200

    add nest and fix parallax

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -28,16 +28,6 @@ const Experience: NextPage<Props> = ({ experiences }) => {
 	const paraRef = useRef<IParallax>(null);
 	const bRef = useRef<HTMLDivElement>(null);
 
-	const handleNextClick = (e, index) => {
-		e.stopPropagation();
-		paraRef.current?.scrollTo(index + 1);
-	};
-
-	const handlePrevClick = (e, index) => {
-		e.stopPropagation();
-		paraRef.current?.scrollTo(index - 1);
-	};
-
 	const iconList = (companyName) => {
 		switch (companyName) {
 			case "Spark Sport":
@@ -63,50 +53,31 @@ const Experience: NextPage<Props> = ({ experiences }) => {
 	return (
 		<Layout>
 			<Center>
-				<Box ref={bRef} id='hello'>
+				<Box ref={bRef}>
 					<Parallax
 						ref={paraRef}
 						pages={experiences.length}
-						enabled={false}
 						style={{
-							top: isMobile ? 50 : 75,
+							top: isMobile ? 50 : 100,
 							left: 0,
-							paddingTop: isMobile ? "16px" : "50px",
+							marginTop: isMobile ? "16px" : "50px",
 							paddingLeft: isMobile ? "25px" : "100px",
 						}}
 						className="parallax"
 					>
-						{experiences.map((experience, index, array) => (
-							<ParallaxLayer key={experience.id} offset={index} speed={0.5}>
-								<Box
-									component="section"
-									id={experience.company_name ?? "comp_name"}
-								>
+						{experiences.map((experience, index) => (
+							<ParallaxLayer
+								key={experience.id}
+								offset={index}
+								factor={1.2}
+								speed={0.5}
+							>
+								<Box component="section" id={experience.id ?? "comp_name"}>
 									<ExperienceCard
 										experience={experience}
 										iconList={iconList(experience.company_name)}
 										gradient={gradient(experience.company_name)}
 									/>
-									<Group>
-										{index !== 0 ? (
-											<Button
-												mt="sm"
-												variant="gradient"
-												gradient={gradient(experience.company_name)}
-												onClick={(e) => handlePrevClick(e, index)}
-											>
-												Previous experience
-											</Button>
-										) : (
-											<Button
-												mt="sm"
-												variant="gradient"
-												onClick={(e) => handleNextClick(e, index)}
-											>
-												Next experience
-											</Button>
-										)}
-									</Group>
 								</Box>
 							</ParallaxLayer>
 						))}
--- src/3DComponents/FloatingIcons/FloatingIcons.tsx
+++ src/3DComponents/FloatingIcons/FloatingIcons.tsx
@@ -1,91 +1,65 @@
-import React from 'react'
-import { Float, Image, Svg, Text } from '@react-three/drei'
-import { isMobile } from 'react-device-detect'
+import React from "react";
+import { Float, Image, Svg, Text } from "@react-three/drei";
+import { isMobile } from "react-device-detect";
 
 const FloatingIcons = () => {
-  return (
-    <Float speed={0.3} position-y={isMobile ? 0 : -0.25}>
-      <Text
-        font="./bangers-v20-latin-regular.woff"
-        fontSize={0.5}
-        position={[0, 1.5, 0]}
-        maxWidth={2}
-        textAlign="center"
-      >
-        Franck Poingt
-      </Text>
-      <Text
-        font="./bangers-v20-latin-regular.woff"
-        fontSize={0.25}
-        position={[0, 0.75, 0]}
-        maxWidth={2}
-        textAlign="center"
-      >
-        Fullstack Developer
-      </Text>
-      <Svg
-        src="./storybook.svg"
-        position={[-2, 3, -2]}
-        scale={0.002}
-      />
-      <Svg
-        src="./node.svg"
-        position={[-0.5, 3.1, -1]}
-        scale={0.0025}
-      />
-      <Svg
-        src="./typescript.svg"
-        position={isMobile ? [0, 0.1, -2] : [2.6, 3, -2]}
-        scale={0.0025}
-      />
-      <Svg
-        src="./react.svg"
-        position={[isMobile ? -1.5 : -2, 1, -0.1]}
-        scale={0.0025}
-      />
-      <Svg
-        src="./postgres.svg"
-        position={[-3.5, 1.9, -1.3]}
-        scale={0.002}
-      />
-      <Svg
-        src="./serverless.svg"
-        position={[1.6, 1.5, -2]}
-        scale={0.002}
-      />
-      <Svg
-        src="./mantine.svg"
-        position={[-1.2, 1.5, -0.5]}
-        scale={0.002}
-      />
-      <Svg
-        src="./mui.svg"
-        position={[-2.5, 1.5, -3]}
-        scale={0.002}
-      />
-      <Svg
-        src="./html.svg"
-        position={[2.8, 0.5, -2]}
-        scale={0.00025}
-      />
-      <Svg
-        src="./three.svg"
-        position={[1, 0.5, -1]}
-        scale={0.003}
-      />
-      <Svg
-        src="./next.svg"
-        position={[isMobile ? 0.8 : 1.2, 3, -1]}
-        scale={0.0025}
-      />
-      {/* eslint-disable-next-line jsx-a11y/alt-text */}
-      <Image
-        url="./emotion.png"
-        transparent
-        position={[1.3, 2.2, -1.7]}
-        scale={0.4} />
-    </Float>
-  )
-}
+	return (
+		<Float speed={0.3} position-y={isMobile ? 0 : -0.25}>
+			<Text
+				font="./bangers-v20-latin-regular.woff"
+				fontSize={0.5}
+				position={[0, 1.5, 0]}
+				maxWidth={2}
+				textAlign="center"
+			>
+				Franck Poingt
+			</Text>
+			<Text
+				font="./bangers-v20-latin-regular.woff"
+				fontSize={0.25}
+				position={[0, 0.75, 0]}
+				maxWidth={2}
+				textAlign="center"
+			>
+				Fullstack Developer
+			</Text>
+			<Svg src="./storybook.svg" position={[-2, 3, -2]} scale={0.002} />
+			<Svg src="./node.svg" position={[-0.5, 3.1, -1]} scale={0.0025} />
+			<Svg
+				src="./typescript.svg"
+				position={isMobile ? [0, 0.1, -2] : [2.6, 3, -2]}
+				scale={0.0025}
+			/>
+			<Svg
+				src="./react.svg"
+				position={[isMobile ? -1.5 : -2, 1, -0.1]}
+				scale={0.0025}
+			/>
+			<Svg src="./postgres.svg" position={[-3.5, 1.9, -1.3]} scale={0.002} />
+			<Svg src="./serverless.svg" position={[1.6, 1.5, -2]} scale={0.002} />
+			<Svg src="./mantine.svg" position={[-1.2, 1.5, -0.5]} scale={0.002} />
+			<Svg src="./mui.svg" position={[-2.5, 1.5, -3]} scale={0.002} />
+			<Svg src="./html.svg" position={[2.8, 0.5, -2]} scale={0.00025} />
+			<Svg src="./three.svg" position={[1, 0.5, -1]} scale={0.003} />
+			<Svg
+				src="./next.svg"
+				position={[isMobile ? 0.8 : 1.2, 3, -1]}
+				scale={0.0025}
+			/>
+			<Svg
+				src="./nest.svg"
+				position={[isMobile ? 0.8 : 2.2, 1.75, -1]}
+				scale={0.03}
+			/>
+			{/* eslint-disable-next-line jsx-a11y/alt-text */}
+			<Image
+				url="./emotion.png"
+				transparent
+				position={[1.3, 2.2, -1.7]}
+				scale={0.4}
+			/>
+		</Float>
+	);
+};
 
-export default FloatingIcons
+export default FloatingIcons;
