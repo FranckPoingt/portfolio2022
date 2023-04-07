6723a2c7e38b3b70721c1cf9416bd7d578fe158d
Franck P <franckpoingt@gmail.com>
2023-04-07 15:14:11 +1200

    fix exp page

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -1,13 +1,11 @@
-import React, { useRef } from "react";
+import React from "react";
 import { ExperienceRecord, getXataClient } from "@src/xata";
 import { SelectedPick } from "@xata.io/client";
 import { GetStaticProps, NextPage } from "next/types";
-import { Box, Button, Center, Group } from "@mantine/core";
+import { Box, Center, Divider } from "@mantine/core";
 import Layout from "@src/Layout/Layout";
 import ExperienceCard from "@src/components/ExperienceCard/ExperienceCard";
 import { SparkIconList, HarmoneyIconList } from "@src/helpers/constants";
-import { IParallax, Parallax, ParallaxLayer } from "@react-spring/parallax";
-import { isMobile } from "react-device-detect";
 
 interface Props {
 	experiences: Readonly<SelectedPick<ExperienceRecord, ["*"]>>[];
@@ -25,9 +23,6 @@ export const getStaticProps: GetStaticProps = async () => {
 };
 
 const Experience: NextPage<Props> = ({ experiences }) => {
-	const paraRef = useRef<IParallax>(null);
-	const bRef = useRef<HTMLDivElement>(null);
-
 	const iconList = (companyName) => {
 		switch (companyName) {
 			case "Spark Sport":
@@ -53,35 +48,24 @@ const Experience: NextPage<Props> = ({ experiences }) => {
 	return (
 		<Layout>
 			<Center>
-				<Box ref={bRef}>
-					<Parallax
-						ref={paraRef}
-						pages={experiences.length}
-						style={{
-							top: isMobile ? 50 : 100,
-							left: 0,
-							marginTop: isMobile ? "16px" : "50px",
-							paddingLeft: isMobile ? "25px" : "100px",
-						}}
-						className="parallax"
-					>
-						{experiences.map((experience, index) => (
-							<ParallaxLayer
+				<Box>
+					{experiences.map((experience, index, array) => (
+						<>
+							<Box
+								component="section"
 								key={experience.id}
-								offset={index}
-								factor={1.2}
-								speed={0.5}
+								id={experience.id ?? "comp_name"}
+								sx={{ height: "fit-content", paddingTop: "1rem" }}
 							>
-								<Box component="section" id={experience.id ?? "comp_name"}>
-									<ExperienceCard
-										experience={experience}
-										iconList={iconList(experience.company_name)}
-										gradient={gradient(experience.company_name)}
-									/>
-								</Box>
-							</ParallaxLayer>
-						))}
-					</Parallax>
+								<ExperienceCard
+									experience={experience}
+									iconList={iconList(experience.company_name)}
+									gradient={gradient(experience.company_name)}
+								/>
+							</Box>
+							{index < array.length - 1 && <Divider />}
+						</>
+					))}
 				</Box>
 			</Center>
 		</Layout>
